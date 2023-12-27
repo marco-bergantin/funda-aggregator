d1a8888b88f887411b52c0eaacad0a22d9675525
marco-bergantin <bergantin.marco.github@gmail.com>
2023-12-27 11:26:43 +0100

    fix typo

--- FundaAggregator/FundaAggregator/Helpers/RetryStrategyOptionsProvider.cs
+++ FundaAggregator/FundaAggregator/Helpers/RetryStrategyOptionsProvider.cs
@@ -5,17 +5,17 @@ namespace FundaAggregator.Helpers;
 
 public static class RetryStrategyOptionsProvider
 {
-    private const int MaxDelayAllowedInSconds = 60;
+    private const int MaxDelayAllowedInSeconds = 60;
 
-    public static RetryStrategyOptions GetOptions(int maxSecondsDelay = MaxDelayAllowedInSconds) =>
+    public static RetryStrategyOptions GetOptions(int maxSecondsDelay = MaxDelayAllowedInSeconds) =>
         new()
         {
             BackoffType = DelayBackoffType.Exponential,
             UseJitter = true,
             ShouldHandle = new PredicateBuilder().Handle<HttpRequestException>(),
             MaxRetryAttempts = 5,
-            MaxDelay = maxSecondsDelay <= 0 || maxSecondsDelay > MaxDelayAllowedInSconds
-                ? throw new ArgumentException($"{nameof(maxSecondsDelay)} must be a positive int lower than {MaxDelayAllowedInSconds}")
+            MaxDelay = maxSecondsDelay <= 0 || maxSecondsDelay > MaxDelayAllowedInSeconds
+                ? throw new ArgumentException($"{nameof(maxSecondsDelay)} must be a positive int lower than {MaxDelayAllowedInSeconds}")
                 : TimeSpan.FromSeconds(maxSecondsDelay)
         };
 }
